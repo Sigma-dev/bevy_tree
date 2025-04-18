5f7d226cc4e64d890f04ade2267a92f14f969b04
Sigma-dev <antonin.duthil@epitech.eu>
2025-04-18 16:37:27 +0200

    better tree

--- assets/leaf.wgsl
+++ assets/leaf.wgsl
@@ -1,12 +1,55 @@
-#import bevy_pbr::forward_io::VertexOutput
-@group(2) @binding(0) var<uniform> material_color: vec4<f32>;
-@group(2) @binding(1) var material_color_texture: texture_2d<f32>;
-@group(2) @binding(2) var material_color_sampler: sampler;
+#import bevy_pbr::{
+    pbr_functions::alpha_discard,
+    pbr_fragment::pbr_input_from_standard_material,
+}
+
+#ifdef PREPASS_PIPELINE
+#import bevy_pbr::{
+    prepass_io::{VertexOutput, FragmentOutput},
+    pbr_deferred_functions::deferred_output,
+}
+#else
+#import bevy_pbr::{
+    forward_io::{VertexOutput, FragmentOutput},
+    pbr_functions::{apply_pbr_lighting, main_pass_post_lighting_processing},
+    pbr_types::STANDARD_MATERIAL_FLAGS_UNLIT_BIT,
+}
+#endif
+
+@group(2) @binding(100) var<uniform> material_color: vec4<f32>;
+@group(2) @binding(101) var material_color_texture: texture_2d<f32>;
+@group(2) @binding(102) var material_color_sampler: sampler;
+
+fn csin(x: f32) -> f32 {
+    return sin(x) + (sin(x * 2) / 2.) + (sin(x * 4.) / 4.);
+}
+
+fn height_noise(x: f32, z: f32, amplitude: f32, freq: f32) -> f32 {
+    return (csin(x * freq) + csin(z * freq)) * amplitude;
+}
 
 @fragment
 fn fragment(
-    mesh: VertexOutput,
-) -> @location(0) vec4<f32> {
-    let alpha = textureSample(material_color_texture, material_color_sampler, mesh.uv).r;
-    return vec4<f32>(0.1, 0.2, 0.1, alpha);
+    in: VertexOutput,
+    @builtin(front_facing) is_front: bool,
+) -> FragmentOutput {
+    var pbr_input = pbr_input_from_standard_material(in, is_front);
+
+    let alpha = textureSample(material_color_texture, material_color_sampler, in.uv).r;
+    if (alpha == 0.) {
+        discard;
+    }
+    let color = vec4<f32>(0.2, 0.4, 0.2, 1.);
+
+    pbr_input.material.base_color = alpha_discard(pbr_input.material, color);
+
+#ifdef PREPASS_PIPELINE
+    let out = deferred_output(in, pbr_input);
+#else
+    var out: FragmentOutput;
+    out.color = apply_pbr_lighting(pbr_input);
+    out.color = main_pass_post_lighting_processing(pbr_input, out.color);
+#endif
+
+    return out;
 }
--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,11 @@ fn main() -> AppExit {
         .add_plugins((
             DefaultPlugins,
             DefaultEditorCamPlugins,
-            MaterialPlugin::<CustomMaterial>::default(),
-            MaterialPlugin::<ExtendedMaterial<StandardMaterial, GroundMaterial>>::default(),
+            MaterialPlugin::<ExtendedMaterial<StandardMaterial, LeafMaterial>>::default(),
         ))
         .add_systems(Startup, setup)
         .run()
 }
-
-#[derive(Resource)]
-struct TreeAsset(Handle<Gltf>);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((Camera3d::default(), EditorCam::default()));
@@ -33,15 +29,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         Transform::default().looking_at(-Vec3::ONE, Vec3::Y),
     ));
 
-    let tree = TreeAsset(asset_server.load(GltfAssetLabel::Scene(0).from_asset("tree.glb")));
-
     commands
         .spawn(SceneRoot(
             asset_server.load(GltfAssetLabel::Scene(0).from_asset("tree.glb")),
         ))
         .observe(replace_materials);
-
-    commands.insert_resource(tree);
 }
 
 fn replace_materials(
@@ -55,8 +47,7 @@ fn replace_materials(
     )>,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut custom_materials: ResMut<Assets<CustomMaterial>>,
-    mut ground2_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, GroundMaterial>>>,
+    mut leaf_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, LeafMaterial>>>,
 ) {
     let root = trigger.entity();
     for (mesh, handle, GltfMaterialName(name)) in children
@@ -68,25 +59,22 @@ fn replace_materials(
         match name.as_str() {
             "M_Leaf_01.004" => {
                 replace = true;
-                /*  commands
-                .entity(mesh)
-                .insert(MeshMaterial3d(custom_materials.add(CustomMaterial {
-                    color: Color::srgb(0.1, 0.4, 0.1).into(),
-                    color_texture: asset_server.load("T_Leaf_01.png"),
-                    alpha_mode: AlphaMode::Blend,
-                }))); */
                 commands
                     .entity(mesh)
-                    .insert(MeshMaterial3d(ground2_materials.add(ExtendedMaterial {
+                    .insert(MeshMaterial3d(leaf_materials.add(ExtendedMaterial {
                         base: StandardMaterial {
                             base_color: Color::WHITE,
                             opaque_render_method: OpaqueRendererMethod::Auto,
                             alpha_mode: AlphaMode::Opaque,
                             cull_mode: None,
+                            reflectance: 0.,
+                            metallic: 0.,
+                            perceptual_roughness: 0.,
+                            diffuse_transmission: 0.6,
                             ..Default::default()
                         },
-                        extension: GroundMaterial {
-                            color: Color::srgb(0.1, 0.4, 0.1).into(),
+                        extension: LeafMaterial {
+                            color: Color::srgb(0.2, 0.6, 0.2).into(),
                             color_texture: asset_server.load("T_Leaf_01.png"),
                             alpha_mode: AlphaMode::Opaque,
                         },
@@ -98,6 +86,8 @@ fn replace_materials(
                     .insert(MeshMaterial3d(materials.add(StandardMaterial {
                         base_color_texture: Some(asset_server.load("T_Bark_Jagged_Albedo.png")),
                         reflectance: 0.,
+                        metallic: 0.,
+                        perceptual_roughness: 0.,
                         ..default()
                     })));
             }
@@ -111,30 +101,8 @@ fn replace_materials(
     }
 }
 
-#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
-struct CustomMaterial {
-    #[uniform(0)]
-    color: LinearRgba,
-    #[texture(1)]
-    #[sampler(2)]
-    color_texture: Handle<Image>,
-    alpha_mode: AlphaMode,
-}
-
-/// The Material trait is very configurable, but comes with sensible defaults for all methods.
-/// You only need to implement functions for features that need non-default behavior. See the Material api docs for details!
-impl Material for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "leaf.wgsl".into()
-    }
-
-    fn alpha_mode(&self) -> AlphaMode {
-        self.alpha_mode
-    }
-}
-
 #[derive(Asset, AsBindGroup, Reflect, Debug, Clone)]
-struct GroundMaterial {
+struct LeafMaterial {
     #[uniform(100)]
     color: LinearRgba,
     #[texture(101)]
@@ -143,12 +111,12 @@ struct GroundMaterial {
     alpha_mode: AlphaMode,
 }
 
-impl MaterialExtension for GroundMaterial {
+impl MaterialExtension for LeafMaterial {
     fn fragment_shader() -> ShaderRef {
-        "ground.wgsl".into()
+        "leaf.wgsl".into()
     }
 
     fn deferred_fragment_shader() -> ShaderRef {
-        "ground.wgsl".into()
+        "leaf.wgsl".into()
     }
 }
